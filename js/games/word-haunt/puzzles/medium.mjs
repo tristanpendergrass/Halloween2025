786caf8901356487c77a02ee191780f2245a287d
John Pendergrass <johnpendergrass@gmail.com>
2025-10-23 16:46:33 -0700

    changed Word Haunt Medium
    rearranged letters in ROCK and CANDY - seems solvable and non-stickable??

--- js/games/word-haunt/puzzles/medium.mjs
+++ js/games/word-haunt/puzzles/medium.mjs
@@ -6,51 +6,64 @@
  */
 
 export default {
-    name: "Trick 'r Treat Goodness",
-    description: "Find spooky words with moderate challenge!",
+  name: "Trick 'r Treat Goodness",
+  description: "Find spooky words with moderate challenge!",
 
-    grid: [
-        ['E', 'N', 'C', 'N', 'R', 'O'],
-        ['Y', 'O', 'N', 'A', 'C', 'B'],
-        ['L', 'M', 'D', 'O', 'P', 'O'],
-        ['L', 'A', 'Y', 'P', 'N', 'U'],
-        ['C', 'F', 'B', 'C', 'R', 'E'],
-        ['A', 'R', 'T', 'Y', 'O', 'L'],
-        ['R', 'U', 'I', 'K', 'C', 'P'],
-        ['A', 'M', 'E', 'L', 'A', 'P'],
-    ],
-    // grid: [
-    //     [' ', ' ', ' ', ' ', ' ', ' '],
-    //     [' ', ' ', ' ', ' ', ' ', ' '],
-    //     [' ', ' ', ' ', ' ', ' ', ' '],
-    //     [' ', ' ', ' ', ' ', ' ', ' '],
-    //     [' ', ' ', ' ', ' ', ' ', ' '],
-    //     [' ', ' ', ' ', ' ', ' ', ' '],
-    //     [' ', ' ', ' ', ' ', ' ', ' '],
-    //     [' ', ' ', ' ', ' ', ' ', ' '],
-    // ],
-    // grid: [
-    //     ['Y', 'D', 'N', 'A', 'C', ' '],
-    //     ['B', 'O', 'U', ' ', ' ', ' '],
-    //     [' ', ' ', ' ', 'N', 'C', ' '],
-    //     [' ', ' ', ' ', ' ', ' ', 'Y'],
-    //     ['C', ' ', ' ', ' ', 'B', 'E'],
-    //     ['A', ' ', ' ', ' ', 'A', 'L'],
-    //     ['R', ' ', ' ', 'L', 'L', 'P'],
-    //     ['A', 'M', 'E', 'L', 'A', 'P'],
-    // ],
+  // old grid- the original w problem solutions
+  // grid: [
+  //    ['E', 'N', 'C', 'N', 'R', 'O'],
+  //    ['Y', 'O', 'N', 'A', 'C', 'B'],
+  //    ['L', 'M', 'D', 'O', 'P', 'O'],
+  //    ['L', 'A', 'Y', 'P', 'N', 'U'],
+  //    ['C', 'F', 'B', 'C', 'R', 'E'],
+  //    ['A', 'R', 'T', 'Y', 'O', 'L'],
+  //    ['R', 'U', 'I', 'K', 'C', 'P'],
+  //    ['A', 'M', 'E', 'L', 'A', 'P'],
+  //],
+  // new grid - maybe will work?
+  grid: [
+    ["E", "N", "Y", "N", "R", "O"],
+    ["Y", "O", "D", "N", "C", "B"],
+    ["L", "M", "A", "O", "P", "O"],
+    ["L", "A", "C", "P", "N", "U"],
+    ["C", "F", "B", "C", "K", "E"],
+    ["A", "R", "T", "Y", "C", "L"],
+    ["R", "U", "I", "R", "O", "P"],
+    ["A", "M", "E", "L", "A", "P"],
+  ],
 
-    // Target words to find - mix of lengths and orientations
-    words: [
-        'CARAMELAPPLE', //
-        'CANDY',
-        'MONEY',
-        'POPCORN', //
-        'FRUIT',//
-        'BOUNCYBALL', //
-        'ROCK', //
-    ],
+  // grid: [
+  //     [' ', ' ', ' ', ' ', ' ', ' '],
+  //     [' ', ' ', ' ', ' ', ' ', ' '],
+  //     [' ', ' ', ' ', ' ', ' ', ' '],
+  //     [' ', ' ', ' ', ' ', ' ', ' '],
+  //     [' ', ' ', ' ', ' ', ' ', ' '],
+  //     [' ', ' ', ' ', ' ', ' ', ' '],
+  //     [' ', ' ', ' ', ' ', ' ', ' '],
+  //     [' ', ' ', ' ', ' ', ' ', ' '],
+  // ],
+  // grid: [
+  //     ['Y', 'D', 'N', 'A', 'C', ' '],
+  //     ['B', 'O', 'U', ' ', ' ', ' '],
+  //     [' ', ' ', ' ', 'N', 'C', ' '],
+  //     [' ', ' ', ' ', ' ', ' ', 'Y'],
+  //     ['C', ' ', ' ', ' ', 'B', 'E'],
+  //     ['A', ' ', ' ', ' ', 'A', 'L'],
+  //     ['R', ' ', ' ', 'L', 'L', 'P'],
+  //     ['A', 'M', 'E', 'L', 'A', 'P'],
+  // ],
 
-    gridSize: 48,
-    totalWordLength: 48,
+  // Target words to find - mix of lengths and orientations
+  words: [
+    "CARAMELAPPLE", //
+    "CANDY",
+    "MONEY",
+    "POPCORN", //
+    "FRUIT", //
+    "BOUNCYBALL", //
+    "ROCK", //
+  ],
+
+  gridSize: 48,
+  totalWordLength: 48,
 };
